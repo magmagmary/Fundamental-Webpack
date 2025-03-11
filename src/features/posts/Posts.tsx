d9e8fb30b6b1befdb0908c2073cd247c34397260
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-03-11 10:33:00 +0100

    fix: base url config for react router

--- src/features/posts/Posts.tsx
+++ src/features/posts/Posts.tsx
@@ -11,7 +11,7 @@ const Posts = () => {
 	if (isError) return <div>Error</div>;
 
 	return (
-		<ul className="grid grid-cols-5 gap-4">
+		<ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
 			{data?.map((post: Post) => (
 				<li
 					key={post.id}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,14 +3,21 @@ import Layout from '@features/layout/Layout';
 import Posts from '@features/posts/Posts';
 import { createBrowserRouter } from 'react-router';
 
-export const router = createBrowserRouter([
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+export const router = createBrowserRouter(
+	[
+		{
+			element: <Layout />,
+			children: [
+				{
+					path: '/',
+					element: <Posts />,
+				},
+			],
+		},
+	],
 	{
-		element: <Layout />,
-		children: [
-			{
-				path: '/',
-				element: <Posts />,
-			},
-		],
+		basename: isDevelopment ? '/' : '/Fundamental-Webpack',
 	},
-]);
+);
